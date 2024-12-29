003814f6aa7442cacc58a22efec8bea0d86abfad
WIE HYANG HOON <gidgns1995@gmail.com>
2024-12-29 19:09:49 +0900

    [add] 삼각형 판별하기

--- section-1/q2.js
+++ section-1/q2.js
@@ -2,12 +2,24 @@
 
 function solution(a, b, c) {
     let answer = "YES";
-
     // 최대값을 담아두는 변수
-    let max;
-    let sum;
+    let max
+    // 파라미터 합계
+    let sum = a + b + c;
 
-    return answer;
+    if (a > b) {
+        max = a;
+    } else {
+        max = b;
+    }
+
+    if (c < max) max = c;
+
+    if((sum-max) > max) {
+        return answer
+    } else {
+        return answer = "NO"
+    }
 }
 
-console.log(solution(6,7,11))
+console.log(solution(12,5,11))
