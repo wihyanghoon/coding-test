5fd37e9b97d5a41b2604d03f73d19d2d7680515d
WIE HYANG HOON <gidgns1995@gmail.com>
2025-01-01 15:12:12 +0900

    A를 #으로 바꿔라

--- section-1/q9.js
+++ section-1/q9.js
@@ -1,28 +1,9 @@
-// 백설공주와 일곱난쟁이
-// 9명의 난쟁이가 나타났다.
-// 원래 7명의 난쟁이는 키의 총합이 100이다.
-// 난쟁이를 찾아라
+// A를 #으로
 
-function solution (arr) {
-    const sum = arr.reduce((a, b)=>a + b, 0);
-    const hobit = [];
+function solution (str) {
+    const result = str.replaceAll("A", "#");
 
-    // 배열 순회
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = i+1; j < arr.length; j++) {
-            // 특정 데이터 값에서 총합을 뺏을때 100인지 확인
-            if(sum - (arr[i] + arr[j]) === 100) {
-                // 맞으면 해당 데이터들을 배열에 담는다.
-                hobit.push(arr[i], arr[j]);
-            }
-        }
-    }
-
-    const result = arr.filter((item, index)=> !hobit.includes(item))
-
-    console.log(result)
+    console.log(result);
 }
 
-
-const arr = [20, 7, 23, 19, 10, 15, 25, 8, 13];
-solution(arr);
+solution("BANANA");
